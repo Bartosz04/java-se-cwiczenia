e4060934df31ed9cc6da1373fcfe50ca69e8dea2
Bartosz04 <bartosz.wisniewski04@gmail.com>
2018-11-09 16:49:44 +0100

    Zakonczenie zajec 9.11.2018

--- workshop/src/Ex6.java
+++ workshop/src/Ex6.java
@@ -4,33 +4,58 @@ import java.util.Scanner;
 public class Ex6 {
     public static void main(String[] args) {
 
-        double firstNumber = readNumberFromConsole(1);
-        int[] table = {2, 4, 6, 8, 10};
-        int[] tableFound = {0, 0, 0, 0, 0};
+        int firstNumber = readNumberFromConsole(1);
+        int[] table = {1,2,3,4,5,6,7,8,9,10};
+        int[] tableFound = {-1, -1, -1, -1, -1,-1, -1, -1, -1, -1};
+        int length = table.length;
 
-        int foundA = check(table,firstNumber,tableFound);
+        int count = check(table, firstNumber, tableFound, length);
 
-        for(int i =0;i<5;i++) {
-            System.out.println(Arrays.toString(tableFound));
-        }
+
+        System.out.println(Arrays.toString(tableFound));
+
+        int[] finalTables = finalTable(table, tableFound, length, count);
+
+        System.out.println(Arrays.toString(finalTables));
+
+
     }
 
-    static int check (int[] table, double firstNumber, int[] tableFound){
-        int count=0;
-        int sum =0;
-        int found=0;
-        for(int i =0;i<5;i++) {
-                sum = table[i] + table[i+1];
-                //found = table[i+1];
-
-                tableFound[count] = table[i+1];
-            count+=1;
-           // return tableFound[i+1];
-            return 0;
+    static int check(int[] table, int firstNumber, int[] tableFound, int length) {
+        int count = 0;
+        int sum = 0;
+        int found = 0;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 1; j < length; j++) {
+                sum = table[i] + table[j];
+                if (firstNumber == sum && i != j && i < j) {
+                    tableFound[j] = table[j];
+                    found = table[j];
+                    System.out.println("znaleziona liczba; " + found);
+                    count += 1;
+                }
+            }
         }
-        return 0;
-       // return tableFound[];
+        return count;
     }
+
+    static int[] finalTable(int[] table, int[] tableFound, int length, int count) {
+        int[] finalTable1 = new int[length - count];
+        int finalTableIterator = 0;
+        for (int i = 0; i < length; i++) {
+            boolean shouldAdd = true;
+            for (int j = 0; j < tableFound.length; j++) {
+                if (table[i] == tableFound[j]) {
+                    shouldAdd = false;
+                }
+            }
+            if (shouldAdd) {
+                finalTable1[finalTableIterator++] = table[i];
+            }
+        }
+        return finalTable1;
+    }
+
 
     static int readNumberFromConsole(int numberInOrder) {
         Scanner scanner = new Scanner(System.in);
--- workshop/src/ExHome1.java
+++ workshop/src/ExHome1.java
@@ -1,28 +1,51 @@
-import  java.util.Arrays;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ExHome1 {
 
     public static void main(String[] args) {
-        int tabel[] = {2,4,6,8,10};
-        int tabelEmpty[] = {0,0,0,0,0};
+        int tabel[] = {2, 4, 6, 8, 10};
+        int[] tableFound = {0, 0, 0, 0, 0};
 
         System.out.println("Podaj liczbe:");
         Scanner read = new Scanner(System.in);
         int number = read.nextInt();
 
-        int sum =0;
+        int length = tabel.length;
+
+        int[] newTabel = new int[length -1];
+
+        int sum = 0;
         int count =0;
 
-        for(int i =0;i<5;i++){
-            for(int j =1;j<5;j++){
-                sum = tabel[i]+tabel[j];
-                if ((i != j) && (i < j) && (sum == number)){
-                    System.out.println("wartosc i: "+tabel[i]);
-                    System.out.println("wartosc j: "+tabel[j]);
+        for (int i = 0; i < length -1; i++) {
+            for (int j = 1; j < length; j++) {
+                sum = tabel[i] + tabel[j];
+                if ((i != j) && (i < j) && (sum == number)) {
+                    System.out.println("wartosc i: " + tabel[i]);
+                    System.out.println("wartosc j: " + tabel[j]);
+
                     count++;
-                    tabelEmpty[j]=tabel[j];
-                    System.out.println(Arrays.toString(tabelEmpty));
+                    System.out.println("ile liczb "+count);
+
+
+                /*    int found = j;
+
+                    System.out.println(found);
+
+                    tabel=new int[length-1];
+
+                   for (i = 0; i < found; i++) {
+                        tabel[i] = tabel[i];
+
+                    }
+                    for (i = found; i < length - 1; i++) {
+                        tabel[i] = tabel[i + 1];
+                    }
+                    System.out.println(Arrays.toString(tabel));
+
+                    length--;
+*/
                 }
             }
         }
@@ -30,6 +53,9 @@ public class ExHome1 {
 
 
 
+
+        }
+
     }
 
-}
+
